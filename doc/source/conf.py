910a72785d3b345566227a3ecfcad1400cc5bd72
cesans <cesanxz@gmail.com>
2016-06-05 16:29:37 +0100

    Fixing docs for rtd.org

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,8 @@ import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
 import mock
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pylab']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pylab', 'sklearn', 'sklearn.cluster',
+		'sklearn.utils', 'sklearn.gaussian_process']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
